d7eacf50093d318f0ae4390eb79a4deb28e7e223
Joshu4l <albertjoshua5@gmail.com>
2022-01-18 16:31:40 +0100

    added a class definition

--- yfinance_exercise.py
+++ yfinance_exercise.py
@@ -1,10 +1,33 @@
 import yfinance as yf
 
-nvidia = yf.Ticker("NVD")
+class Stock:
+	def __init__(self, stock_abbreviation):
+		self.abbreviation = stock_abbreviation
+		self.ticker = yf.Ticker(stock_abbreviation)
+		self.info = self.ticker.info
+		self.news = self.ticker.news
+		self.history = self.ticker.history(period="max",interval="1mo")
+		# possible parameter settings for <interval>:
+		#	"1d"
+		#	"1wk"
+		#	"1mo"
+		#	"1j"
+		#	"ytd"
+		#	"max"
+		# print(hist["Open"], hist["Close"])
 
-# get stock info
-print(nvidia.info)
-print(type(nvidia.info))
+	def __str__(self):
+		print(f"some info about {self.abbreviation}")
 
-# get historical market data
-hist = nvidia.history(period="max")
+
+# for key, value in nvidia
+# for news_item in nvidia.news:
+	#print(news_item)
+	# print(news_item["providerPublishTime"], news_item["title"], news_item["link"])
+
+nvd = Stock("NVD")
+print(nvd.info)
+
+
+print(nvd.history)
+
